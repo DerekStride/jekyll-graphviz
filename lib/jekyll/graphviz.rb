7d2c555cfc0ee4a37bec7d2b33496d003e50ffa2
derekstride <djgstride@gmail.com>
2021-07-24 09:35:23 -0400

    Initial commit: bundle gem

---
+++ lib/jekyll/graphviz.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "graphviz/version"
+
+module Jekyll
+  module Graphviz
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
